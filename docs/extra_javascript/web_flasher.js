5aaf6745e46af2db89361632633f19d8fabe16b1
Christian Baars <christianbaars@MacBook-Pro-von-Christian.local>
2022-04-01 08:36:56 +0200

    more fixes

--- docs/extra_javascript/web_flasher.js
+++ docs/extra_javascript/web_flasher.js
@@ -5,6 +5,7 @@ s=document.createElement('script');s.type='module';s.src="https://unpkg.com/esp-
 document.body.append(s);
 b=document.createElement("esp-web-install-button");b.manifest="https://tasmota.github.io/install/manifest/release.tasmota.manifest.json";
 
+
 p=document.createElement('div');
 sel=document.createElement('select');
 sel.classList.add('pick-variant');
@@ -37,35 +38,35 @@ function make_select(data){
 
 
 function appendInTable(){
+//Step1: add button to DOM
 const anchor_point =document.getElementById("web_installer").parentElement.nextElementSibling.nextElementSibling.children[5].firstElementChild.firstElementChild;
-// console.log(b.shadowRoot.firstChild.name);
-// anchor_point.parentNode.append(p);
 anchor_point.parentNode.append(b);
-const selectEl = document.querySelector(".pick-variant");
-const installEl = document.querySelector("esp-web-install-button");
-installEl.manifest = selectEl.value;
-selectEl.addEventListener("change", () => {
-    installEl.manifest = selectEl.value;
-    console.log(installEl.manifest);
-});
 }
 
 window.addEventListener("load", function(event) {
-    const button = document.querySelector("esp-web-install-button");
-    // const unsupp = document.querySelector("esp-web-install-button").shadowRoot.firstChild.name
-    console.log(button.shadowRoot.firstChild.name);
-    if(button.shadowRoot.firstChild.name == 'activate'){
-        // $("esp-web-install-button").before(p);
-        button.insertAdjacentHTML('beforebegin','<ol><li> <p>Connect the ESP device to your computer using USB or serial-to-USB adapter</p> </li><li> <p>Select the firmware variant suitable for your device</p> </li></ol>');
-        // button.insertAdjacentHTML('beforebegin','<br>');
-        button.insertAdjacentElement('beforebegin',p);
-        button.insertAdjacentHTML('beforebegin','<ol><li> <p>Hit "CONNECT" and select the correct port or find help if no device found</p> </li></ol>');
-        // button.insertAdjacentHTML('beforebegin','<br>');
-    }
-},{ once: true });
+    //step2: check result of attempt to add button to DOM
+    try{
+        document.body.style.cssText = "--mdc-typography-body1-font-size:1.5em;" //smaller text for Jason8266 ;)
+        const button = document.querySelector("esp-web-install-button");
+        console.log(button.shadowRoot.firstChild.name);
+        if(button.shadowRoot.firstChild.name == 'activate'){
+            //success: add the select picker and some info
+            button.insertAdjacentHTML('beforebegin','<p>1. Connect the ESP device to your computer using USB or serial-to-USB adapter</p><p>2. Select the firmware variant suitable for your device</p>');
+            button.insertAdjacentElement('beforebegin',p);
+            button.insertAdjacentHTML('beforebegin','<p>3. Hit "CONNECT" and select the correct port or find help if no device found</p>');
+            const selectEl = document.querySelector(".pick-variant");
+            button.manifest = selectEl.value;
+            selectEl.addEventListener("change", () => {
+                button.manifest = selectEl.value;
+                console.log(button.manifest);
+            });
+        }
 
-{/* <ol>
-<li> <p>Connect the ESP device to your computer using USB or serial-to-USB adapter</p> </li>
-<li> <p>Select the firmware variant suitable for your device</p> </li>
-<li> <p>Hit "Install" and select the correct port or find help if no device found</p> </li>
-</ol> */}
+    }
+    catch(e){
+        const anchor_point =document.getElementById("web_installer").parentElement.nextElementSibling.nextElementSibling.children[5].firstElementChild.firstElementChild;
+        anchor_point.insertAdjacentHTML('afterbegin','<p>Unsupported platform/browser. Use another flashing method.</p>');
+        console.log(e);
+    }
+
+},{ once: true });
